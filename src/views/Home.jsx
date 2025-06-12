135010006d91dc190fdc75503e740f67c05de714
oniids <jdino.abaya@gmail.com>
2025-06-12 14:44:09 +0800

    add hero section on homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>This is the home page</div>;
+  return (
+    <section className="hero-section bg-gradient-to-r from-tertiary to-secondary text-light text-center">
+      <div className="hero-container">
+        <h1 className="hero-title text-4xl font-extrabold">
+          Welcome to Cineverse
+        </h1>
+        <p className="hero-text  font-extralight">
+          Discover thousands of movies and TV shows, get detailed information,
+          and stay updated with what's trending.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
